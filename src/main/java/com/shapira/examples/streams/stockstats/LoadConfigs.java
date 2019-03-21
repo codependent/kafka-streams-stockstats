74c6e30f53bf061b7a5abda181b3768bb0b5f683
Viktor Gamov <viktor@confluent.io>
2019-03-20 17:12:21 -0700

    Minor refactoring
    - updated dependencies
    - updated readme
    - some code simplification

--- src/main/java/com/shapira/examples/streams/stockstats/LoadConfigs.java
+++ src/main/java/com/shapira/examples/streams/stockstats/LoadConfigs.java
@@ -10,29 +10,29 @@ import java.util.Properties;
 
 /**
  * Load configuration from a file. This is mainly intended for connection info, so I can switch between clusters without recompile
- * But you can put other client configation here, but we may override it...
+ * But you can put other client configuration here, but we may override it...
  */
 public class LoadConfigs {
 
-    // default to cloud, duh
-    private static final String DEFAULT_CONFIG_File =
-            System.getProperty("user.home") + File.separator + ".ccloud" + File.separator + "config";
+  // default to cloud, duh
+  private static final String DEFAULT_CONFIG_File =
+      System.getProperty("user.home") + File.separator + ".ccloud" + File.separator + "config";
 
-    static Properties loadConfig() throws IOException {
-        return loadConfig(DEFAULT_CONFIG_File);
+  static Properties loadConfig() throws IOException {
+    return loadConfig(DEFAULT_CONFIG_File);
+  }
+
+  static Properties loadConfig(String configFile) throws IOException {
+    if (!Files.exists(Paths.get(configFile))) {
+      throw new RuntimeException(configFile + " does not exist. You need a file with client configuration, " +
+                                 "either create one or run `ccloud init` if you are a Confluent Cloud user");
+    }
+    System.out.println("Loading configs from:" + configFile);
+    final Properties cfg = new Properties();
+    try (InputStream inputStream = new FileInputStream(configFile)) {
+      cfg.load(inputStream);
     }
 
-    static Properties loadConfig(String configFile) throws IOException {
-        if (!Files.exists(Paths.get(configFile))) {
-            throw new RuntimeException(configFile + " does not exist. You need a file with client configuration, " +
-                    "either create one or run `ccloud init` if you are a Confluent Cloud user");
-        }
-        System.out.println("Loading configs from:" + configFile);
-        final Properties cfg = new Properties();
-        try (InputStream inputStream = new FileInputStream(configFile)) {
-            cfg.load(inputStream);
-        }
-
-        return cfg;
-    }
+    return cfg;
+  }
 }
--- src/main/java/com/shapira/examples/streams/stockstats/StockStatsExample.java
+++ src/main/java/com/shapira/examples/streams/stockstats/StockStatsExample.java
@@ -1,20 +1,21 @@
 package com.shapira.examples.streams.stockstats;
 
+import com.shapira.examples.streams.stockstats.model.Trade;
+import com.shapira.examples.streams.stockstats.model.TradeStats;
 import com.shapira.examples.streams.stockstats.serde.JsonDeserializer;
 import com.shapira.examples.streams.stockstats.serde.JsonSerializer;
 import com.shapira.examples.streams.stockstats.serde.WrapperSerde;
-import com.shapira.examples.streams.stockstats.model.Trade;
-import com.shapira.examples.streams.stockstats.model.TradeStats;
+
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.DescribeClusterResult;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.TimeWindows;
@@ -24,6 +25,9 @@ import org.apache.kafka.streams.state.WindowStore;
 
 import java.util.Properties;
 
+import static com.shapira.examples.streams.stockstats.Constants.STATS_OUT_TOPIC;
+import static com.shapira.examples.streams.stockstats.Constants.STOCK_TOPIC;
+
 /**
  * Input is a stream of trades
  * Output is two streams: One with minimum and avg "ASK" price for every 10 seconds window
@@ -31,74 +35,79 @@ import java.util.Properties;
  */
 public class StockStatsExample {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        Properties props;
-        if (args.length==1)
-            props = LoadConfigs.loadConfig(args[0]);
-        else
-            props = LoadConfigs.loadConfig();
-
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stockstat-2");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, TradeSerde.class.getName());
-
-        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
-        // Note: To re-run the demo, you need to use the offset reset tool:
-        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // creating an AdminClient and checking the number of brokers in the cluster, so I'll know how many replicas we want...
-
-        AdminClient ac = AdminClient.create(props);
-        DescribeClusterResult dcr = ac.describeCluster();
-        int clusterSize = dcr.nodes().get().size();
-
-        if (clusterSize<3)
-            props.put("replication.factor",clusterSize);
-        else
-            props.put("replication.factor",3);
-
-        StreamsBuilder builder = new StreamsBuilder();
-
-        KStream<String, Trade> source = builder.stream(Constants.STOCK_TOPIC);
-
-        KStream<Windowed<String>, TradeStats> stats = source
-                .groupByKey()
-                .windowedBy(TimeWindows.of(5000).advanceBy(1000))
-                .<TradeStats>aggregate(() -> new TradeStats(),(k, v, tradestats) -> tradestats.add(v),
-                        Materialized.<String, TradeStats, WindowStore<Bytes, byte[]>>as("trade-aggregates")
-                                .withValueSerde(new TradeStatsSerde()))
-                .toStream()
-                .mapValues((trade) -> trade.computeAvgPrice());
-
-        stats.to("stockstats-output", Produced.keySerde(WindowedSerdes.timeWindowedSerdeFrom(String.class)));
-
-        Topology topology = builder.build();
-
-        KafkaStreams streams = new KafkaStreams(topology, props);
-
-        System.out.println(topology.describe());
-
-        streams.cleanUp();
-
-        streams.start();
-
-        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
+    Properties props;
+    if (args.length == 1) {
+      props = LoadConfigs.loadConfig(args[0]);
+    } else {
+      props = LoadConfigs.loadConfig();
     }
 
-    static public final class TradeSerde extends WrapperSerde<Trade> {
-        public TradeSerde() {
-            super(new JsonSerializer<Trade>(), new JsonDeserializer<Trade>(Trade.class));
-        }
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stockstat-2");
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, TradeSerde.class.getName());
+
+    // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+    // Note: To re-run the demo, you need to use the offset reset tool:
+    // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    // creating an AdminClient and checking the number of brokers in the cluster, so I'll know how many replicas we want...
+
+    AdminClient ac = AdminClient.create(props);
+    DescribeClusterResult dcr = ac.describeCluster();
+    int clusterSize = dcr.nodes().get().size();
+
+    if (clusterSize < 3) {
+      props.put("replication.factor", clusterSize);
+    } else {
+      props.put("replication.factor", 3);
     }
 
-    static public final class TradeStatsSerde extends WrapperSerde<TradeStats> {
-        public TradeStatsSerde() {
-            super(new JsonSerializer<TradeStats>(), new JsonDeserializer<TradeStats>(TradeStats.class));
-        }
+    StreamsBuilder builder = new StreamsBuilder();
+
+    KStream<String, Trade> source = builder.stream(STOCK_TOPIC);
+
+    KStream<Windowed<String>, TradeStats> stats = source
+        .groupByKey()
+        .windowedBy(TimeWindows.of(5000).advanceBy(1000))
+        .aggregate(TradeStats::new,
+                   (k, v, tradestats) -> tradestats.add(v),
+                   Materialized.<String, TradeStats, WindowStore<Bytes, byte[]>>as("trade-aggregates")
+                       .withValueSerde(new TradeStatsSerde()))
+        .toStream()
+        .mapValues(TradeStats::computeAvgPrice);
+
+    stats.to(STATS_OUT_TOPIC,
+             Produced.keySerde(WindowedSerdes.timeWindowedSerdeFrom(String.class)));
+
+    Topology topology = builder.build();
+    KafkaStreams streams = new KafkaStreams(topology, props);
+
+    System.out.println(topology.describe());
+
+    streams.cleanUp();
+
+    streams.start();
+
+    // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+  }
+
+  static public final class TradeSerde extends WrapperSerde<Trade> {
+
+    public TradeSerde() {
+      super(new JsonSerializer<Trade>(), new JsonDeserializer<Trade>(Trade.class));
     }
+  }
+
+  static public final class TradeStatsSerde extends WrapperSerde<TradeStats> {
+
+    public TradeStatsSerde() {
+      super(new JsonSerializer<TradeStats>(), new JsonDeserializer<TradeStats>(TradeStats.class));
+    }
+  }
 
 }
